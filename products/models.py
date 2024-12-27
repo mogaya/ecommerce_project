a644c130ab77b30d988bf7bc706efea58ef6d092
mogaya <mogayainnocent@gmail.com>
2024-12-27 14:05:04 +0300

    CRUD on products app using ViewSets and Routers

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    stock_quantity = models.IntegerField()
+    category = models.CharField(max_length=50)
+    image_url = models.URLField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+# Product ViewSet
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_date')
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+    
