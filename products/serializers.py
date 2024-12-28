316475266331b6c3cb1a814c486c711c199f2cc5
mogaya <mogayainnocent@gmail.com>
2024-12-28 17:45:46 +0300

    added validation for name and stock quantity

--- products/serializers.py
+++ products/serializers.py
@@ -10,3 +10,20 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than zero")
         return value
+    
+    def validate_stock_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Stock Quantity must be greater than zero")
+        return value
+    
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name cannot be empty")
+        
+        if len(value) < 3:
+            raise serializers.ValidationError("Name must be atleast 3 characters long")
+        
+        if len(value) > 30:
+            raise serializers.ValidationError("Name cannot exceed 30 characters")
+        
+        return value
