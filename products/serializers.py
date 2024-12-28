12beae33705721774427d75e6656b95a2021fc11
mogaya <mogayainnocent@gmail.com>
2024-12-28 17:28:57 +0300

    added validation for price

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than zero")
+        return value
